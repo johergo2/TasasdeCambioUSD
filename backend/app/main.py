7da8e75d8b0ac743a080187e0297ca41794ec2ab
johergo2 <johergo2@gmail.com>
2026-01-15 11:38:51 -0500

    Ajuste a main.py ubicación v1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import tasas
+from backend.app.routers import tasas
 from routers import tasas_cambio
 
 app = FastAPI(
