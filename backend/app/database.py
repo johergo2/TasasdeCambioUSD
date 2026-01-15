d90af5a8930d05c84c3603baad3970dafdaa82f1
johergo2 <johergo2@gmail.com>
2026-01-15 12:42:41 -0500

    Ajuste a database.py DATABASE_URL v1

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,10 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL",
-                         "postgresql://usuario_api:qLOEsZzJHzqjNBEQDuwEjGXL2dGU4Aq5@dpg-d4s5b77diees73dkljd0-a.ohio-postgres.render.com/db_api_usuarios")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL is None:
-    raise ValueError("La variable de entorno DATABASE_URL no está definida")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL no está definida")
 
 # Ajuste necesario para asyncpg
 DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
